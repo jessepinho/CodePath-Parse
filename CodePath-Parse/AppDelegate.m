506c8191f9967724eab273557178cd215d024238
Jesse Pinho <jesse@jessepinho.com>
2015-11-05 16:47:41 -0600

    Register Parse in AppDelegate

--- CodePath-Parse/AppDelegate.m
+++ CodePath-Parse/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +17,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [Parse setApplicationId:@"DXsvTSLgsKT03gSSqy6V5KbLwVpgfEjmEsKzzQUP" clientKey:@"BXAzmCJhMtIVWhLVEiKIMzPCA5XI0Nt9NwvAOPVd"];
     return YES;
 }
 
