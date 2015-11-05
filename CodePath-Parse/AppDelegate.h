c33bcd4d0d6e8605778e3b694923448beec77976
Jesse Pinho <jesse@jessepinho.com>
2015-11-05 16:45:05 -0600

    Initial Commit

---
+++ CodePath-Parse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodePath-Parse
+//
+//  Created by Jesse Pinho on 11/5/15.
+//  Copyright © 2015 Jesse Pinho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
